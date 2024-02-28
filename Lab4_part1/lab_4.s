eae76427aaca9bd8d38dec247231d435b3695594
ThomasJM03 <99485764+ThomasJM03@users.noreply.github.com>
2024-02-28 09:40:42 -0500

    Update lab_4.s
    This is what I had that worked with the LED

--- Lab4_part1/lab_4.s
+++ Lab4_part1/lab_4.s
@@ -1,4 +1,4 @@
-	.text
+.text
 	.global uart_init
 	.global gpio_btn_and_LED_init
 	.global output_character
@@ -11,12 +11,21 @@
 	.global read_tiva_push_button
 	.global div_and_mod
 	.global lab4
+	.global portINIT
 
 lab4:
 	PUSH {r4-r12,lr}
 	BL uart_init
 	;TESTING LOOP
 	BL gpio_btn_and_LED_init
+
+	MOV r0, #6
+	BL portINIT ;port adress in r1 currently
+	MOV r0, r1	;swaps port address to be in r0
+	MOV r1, #5	;Sets color to be white
+	BL illuminate_RGB_LED
+	MOV r1, #5	;Sets color to be white
+	BL illuminate_RGB_LED
 LOOP:
 	BL read_tiva_push_button
 	B LOOP

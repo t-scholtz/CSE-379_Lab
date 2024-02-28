ca40cd4ee3858f909df35648f6a615f355bfac26
Tim <77660429+t-scholtz@users.noreply.github.com>
2024-02-28 17:02:19 -0500

    Update lab_4.s

--- Lab4_part1/lab_4.s
+++ Lab4_part1/lab_4.s
@@ -1,4 +1,4 @@
-.text;IS THIS SUPPOSED TO BE UP HERE?
+	.data
 	.global uart_init
 	.global gpio_btn_and_LED_init
 	.global output_character
@@ -10,13 +10,7 @@
 	.global illuminate_RGB_LED
 	.global read_tiva_push_button
 	.global div_and_mod
-	.global portINIT
-	.global colorPromptOUT
-	.global newLinePromptOUT
-	.global askRunAgainPromptOUT
-	.global extmsgPromptOut
-	.global startPromptOUT
-	.global startOUT
+	.global lab4
 
 colorPrompt:	.string "Enter number for color:\r\nWhite: 1\r\nRed: 2\r\nGreen: 3\r\nBlue: 4\r\nPurple: 5\r\nYellow: 6\r\n", 0
 startPrompt:	.string "Lab 4 - Tom and Tim"
@@ -31,45 +25,13 @@ ptr_to_runAgin:			.word askRunAgain
 ptr_to_extmsg:			.word extmsg
 
 lab4:
-	;THIS IS THE WORKING GUI
-	;sense there is 4 push buttons and 4 LED's we will give the user 4 chances to do something before we ask if they would like to continue or not
 	PUSH {r4-r12,lr}
 	BL uart_init
-<<<<<<< HEAD
 	LDR r0, ptr_to_startPromt
 	MVN r1,# 1				;new line at end of str
 	BL output_string
 	;TESTING LOOP
 	BL gpio_btn_and_LED_init
-=======
-	BL gpio_btn_and_LED_init;initilization complete
-
-	;Starting up
-	BL startOUT
-	BL newLinePromptOUT
-	BL startPromptOUT
-	
-	;IlluminationLoop
-	MOV r9, #0;Reset the counter to make sure nothing is in it
-illuminationLoop:
-	;BEFORE I CONTINUE WE NEED TO FIGURE OUT HOW TO ACCESS THE OTHER ON BOARD LED's AND BUTTONS
-	;BL colorPromptOUT
-	
-	ADD r9, r9, #1;increment counter
-	
-	
-
-	
-
-	;THIS IS THE TEST STUFF
-	MOV r0, #6
-	BL portINIT ;port adress in r1 currently
-	MOV r0, r1	;swaps port address to be in r0
-	MOV r1, #5	;Sets color to be white
-	BL illuminate_RGB_LED
-	MOV r1, #5	;Sets color to be white
-	BL illuminate_RGB_LED
->>>>>>> 1f945365c892ea954ba58ee91aa8e1c0c530b494
 LOOP:
 	BL read_tiva_push_button
 	B LOOP

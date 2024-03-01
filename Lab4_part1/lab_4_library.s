b9e506f59bdcab0af862b455ee99182a0effa9fe
ThomasJM03 <99485764+ThomasJM03@users.noreply.github.com>
2024-03-01 13:52:41 -0500

    Update lab_4_library.s
    made changed to my LED subs

--- Lab4_part1/lab_4_library.s
+++ Lab4_part1/lab_4_library.s
@@ -1,5 +1,4 @@
 	.data
-
 	.global uart_init
 	.global gpio_btn_and_LED_init
 	.global output_character
@@ -11,490 +10,139 @@
 	.global illuminate_RGB_LED
 	.global read_tiva_push_button
 	.global div_and_mod
+	.global lab4
 	.global int2string
 	.global string2int
 	.global ERRORFOUND
 	.global LOOP
+	.global colorPromptOUT
 
-errorPrompt:	.string "Error with subroutine", 0 ;Question to see if we can do this in this file
-newLine:		.byte 0x0D, 0x0A , 0x00,  0x00
+
+colorPrompt:	.string "Enter number for color:",0x0D, 0x0A,"White: 1",0x0D, 0x0A,"Red: 2",0x0D, 0x0A,"Green: 3",0x0D, 0x0A,"Blue: 4",0x0D, 0x0A,"Purple: 5",0x0D, 0x0A,"Yellow: 6",0x0D, 0x0A, 0
+programPrompt   .string "Enter number for program:",0x0D, 0x0A,"illuminate_RGB_LED: 1",0x0D, 0x0A,"illuminate_LEDs: 2",0x0D, 0x0A,"read_tiva_push_button: 3",0x0D, 0x0A,"read_from_push_btns: 4",0x0D, 0x0A,"Exit Program: 5",0
+startPrompt:	.string "Lab 4 - Tom and Tim",0
+answers			.string "Store answers here",0
+askRunAgain:	.string "Would you like to run again Yes(Y) No(N)?", 0
+extmsg:			.string "End of program ※\(^o^)/※", 0
+btnPrompt: 		.string "Checking if button was pressed",0
+btnPressed:		.string "You Pressed the button!",0
+btnNotPressed:	.string "The button was untouched",0
+usrInput:		.string "buffer for user input",0
 
 	.text
 
-ptr_to_errorPrompt:		.word errorPrompt
-ptr_to_newLine:			.word newLine
+ptr_to_colorPrompt:		.word colorPrompt
+ptr_to_programPrompt:	.word programPrompt
+ptr_to_startPromt:		.word startPrompt
+ptr_to_answers:			.word answers
+ptr_to_runAgin:			.word askRunAgain
+ptr_to_extmsg:			.word extmsg
+ptr_to_btnPrompt:		.word btnPrompt
+ptr_to_btnPressed:		.word btnPressed
+ptr_to_btnNotPressed:	.word btnNotPressed
+ptr_to_usrInput:		.word usrInput
 
-U0FR: 		.equ 0x18	; UART0 Flag Register
-GPIODATA:	.equ 0x3FC ;data location
-GPIODIG:	.equ 0x51C ;Pin activation location
-GPIODIR:	.equ 0x400  ;Pin Direction location
-GPIOPUR: 	.equ 0x510	;Pull-Up Resistor
-
-
-;----------------------------------------------------------------
-;GPIO Button and LED INI - initliase on board button and led for
-;use
-;----------------------------------------------------------------
-gpio_btn_and_LED_init:
+lab4:
 	PUSH {r4-r12,lr}
-	;SET CLCK TO 000
-	MOV r4,#0xE608
-	MOVT r4, #0x400F	;load clck memeory address
-	MOV r0, #0x0000
-	STRB r0, [r4]
-	;SET BUTTON - SW1 - Port F Pin 4 - read | SET LED - Port F pin 1,2,3 - write
-	MOV r0, #32			;port f
-	MOV r1, #0x5000		;port f memory address
-	MOVT r1 , #0x4002
-	MOV r2, #0x0E		;Pin 4 will be read - set 0 | pin 1,2,3 will be write - set 1
-	MOV r3, #0x1E		;Pin 1,2,3,4 set active
-	BL gpio_setup
-	MOV r0, #0x10		;pull up registors
-	STRB r3, [r1,#GPIOPUR]
-	;SET BUTTONS ON ALICE Swithes 2-5 - port D Pins 0-3
-	MOV r0, #8			;port d
-	MOV r1, #0x7000		;port d memory address
-	MOVT r1 , #0x4000
-	MOV r2, #0x00		;Pin 2-5 will be read - set 0
-	MOV r3, #0x0		;Pin 2-5 set active
-	BL gpio_setup
-	;SET LEDS ON ALICE - port B Pins 0-3
-	MOV r0, #2			;port b
-	MOV r1, #0x5000		;port b memory address
-	MOVT r1 , #0x4000
-	MOV r2, #0x0F		;Pin 0-3 will be write - set 1
-	MOV r3, #0x0F		;Pin 0-3 set active
-	BL gpio_setup
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	;INIT
+	BL uart_init
+	BL gpio_btn_and_LED_init
 
-;----------------------------------------------------------------
-;GPIO setup - help method to setup gpio communication
-;Input: r0 - port (A-F|0-5)
-;		r1 - port memory address
-;		r2 - set pins as read or write (8bit input)[0-read 1-write]
-;		r3 - set which pins are active (8bit input)[0-off  1-on]
-;----------------------------------------------------------------
-gpio_setup:
-	PUSH {r4-r12,lr}
-	;SET CLOCK
-	MOV r4,#0xE608
-	MOVT r4, #0x400F	;load clck memeory address
-	LDRB r9, [r4]
-	ORR r0,r0,r9
-	NOP
-	STRB r0, [r4]		;store new settings
-	NOP
-	NOP
-	;SET DIRECTION FOR EACH PIN		;offset for data direction
-	STRB r2, [r1, #GPIODIR]	;store new settings
-	;SET EACH GPIO PIN AS DIGITAL
-	STRB r3, [r1,#GPIODIG]	;store new settings
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	LDR r0, ptr_to_startPromt
+	MVN r1, #1					;new line at end of str
+	BL output_string
 
-;----------------------------------------------------------------
-;read_from_push_btns - reads the momentary push buttons
-;Output: r0 - 4bit value with MSB -> button 2, LSB -> button 5
-;----------------------------------------------------------------
-read_from_push_btns:
-	PUSH {r4-r12,lr}
-	MOV r0,#3
-	BL portINIT
-	LDRB r0, [r4,#GPIODATA]
-	AND r0, #0x3C 		;convert it so 1 is off and 0 is on
-	EOR r0,r0, #0x3C
-	LSR r0,r0,#2
-	;TODO flip bits around right now 5,4,3,2  - need 2,3,4,5
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	;We want to give the user the option to test functionality
+LOOP:
+	LDR r0, ptr_to_programPrompt
+	MVN r1, #1					;new line at end of str
+	BL output_string
+	LDR r0, ptr_to_usrInput
+	BL read_string
+	LDR r0, ptr_to_usrInput
+	BL string2int			;Get usr input and convert it to a number
 
-;----------------------------------------------------------------
-;illuminate_LEDs:	-illuminates the sister board LEDs
-;			input   -half a byte in r0 describing what LEDs to light up
-;			output	-LED light
-;----------------------------------------------------------------
-illuminate_LEDs:
-	PUSH {r4-r12,lr}
-	MOV r4, r0			;keep safe this is the half byte
-	MOV r0, #1			;port B
-	BL portINIT			;B address in reg R1
-	;Store LED data
-	LDRB r2, [r1, #GPIODATA] ;grabs data
-	EOR r2, r2, r4			   ;this will give us new pins to be lit up or not
-	STRB r2, [r1, #GPIODATA] ;stores the data to the same place
+	CMP r0, #1
+	BEQ colourSelector
 
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	CMP r0, #2
+	BEQ onBoardLEDsOUT
 
-;----------------------------------------------------------------
-;illuminate_RGB_LED: - illuminates the RBG LED
-;	input - R0 will be the address of the colors number passed through.//NEEDS TO BE F FOR THIS ONE
-;		  - R1 input color
-;----------------------------------------------------------------
-illuminate_RGB_LED:
-	PUSH {r4-r12,lr};The color from the colorPrompt is passed into r0 ;WE will need a color prompt sub routine too
-	CMP r1, #0;checks error ;Decide with tim if we should loop in here or only print out the message and continue
-	BLS ERRORFOUND;This should be an end of the road subroutine so it will stop us if there is an issue
-	;If white
-	CMP r1, #1
-	BEQ whiteOUT
-	;If red
-	CMP r1, #2
-	BEQ redOUT
-	;If green
-	CMP r1, #3
-	BEQ greenOUT
-	;If blue
-	CMP r1, #4
-	BEQ blueOUT
-	;If purple
-	CMP r1, #5
-	BEQ purpleOUT
-	;If yellow
-	CMP r1, #6
-	BEQ yellowOUT
-	B ERRORFOUND;this is if no options were found; Mabye create a user error sub???
+	CMP r0, #3
+	BEQ buttonBitOUT
 
-whiteOUT:
-	MOV r1, #0xE ;should be 1110
-	B FINALilluminate_RGB_LED
-
-redOUT:
-	MOV r1, #0x2 ;should be 0010
-	B FINALilluminate_RGB_LED
-
-greenOUT:
-	MOV r1, #0x8 ;should be 1000
-	B FINALilluminate_RGB_LED
-
-blueOUT:
-	MOV r1, #0x4 ;should be 0100
-	B FINALilluminate_RGB_LED
-
-purpleOUT:
-	MOV r1, #0x6 ;should be 0110
-	B FINALilluminate_RGB_LED
-
-yellowOUT:
-	MOV r1, #0xC ;should be 1100 ;i have NO CLUE IF I AM ACCESSING THE PINS CORRECTLY
-	B FINALilluminate_RGB_LED
-
-FINALilluminate_RGB_LED:
-	STRB r1, [r0, #GPIODATA]
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;Read SW1 button - return value in r0 - 1 if button is pressed
-;----------------------------------------------------------------
-read_tiva_push_button:
-	PUSH {r4-r12,lr}
-	MOV r4, #0x5000		;port f memory address
-	MOVT r4 , #0x4002
-	LDRB r0, [r4,#GPIODATA]
-	AND r0, #0x10 		;convert it so 1 is off and 0 is on
-	EOR r0,r0, #0x10
-	LSR r0,r0,#4
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;DIV and MOD -  r0/r1 = r0 remainder r1
-;Accepts a dividend in r0 and a divsor in r1and an integer
-;returns the quotient in r0 and the remainder in r1.
-;----------------------------------------------------------------
-div_and_mod:
-	PUSH {r4-r12,lr}
-    MOV r4, r1 ;Sets the temp vals, this is the divisor
-    MOV r5, r0 ;Sets the temp vals, this is the number to divide, the remainder is stored in r1
-    MOV r2, #0 ;resets this the counter returned in r0
-    MOV r6, #0 ;resets
-    CMP r4, #0 ;comparing it to zero
-    BGT NEGCHECK
-    EOR r4, r4, #0xFFFFFFFF ;flips bits for the divisor
-    ADD r4, r4, #1 ;adds 1 for twos comp
-    ADD r6, r6, #1 ;For the negative sign
-NEGCHECK:
-    CMP r5, #0
-    BGT MOD_DIV_LOOP
-    EOR r5, r5, #0xFFFFFFFF ;flips bits for the divided
-    ADD r5, r5, #1
-    ADD r6, r6, #1 ;For the negative sign
-MOD_DIV_LOOP:
-    CMP r5, r4
-    BLT MOD_DIV_DONE
-    SUB r5, r5, r4
-    ADD r2, r2, #1;counting up everytime something divides
-    B MOD_DIV_LOOP
-MOD_DIV_DONE:;checks for negative in and flip to get correct output
-    CMP r6, #1
-	BNE MOD_DIV_NOT_NEG
-    EOR r2, r2, #0xFFFFFFFF ;flips bits for the divisor
-    ADD r2, r2, #1 ;adds 1 for twos comp
-MOD_DIV_NOT_NEG:
-    MOV r0, r2
-    MOV r1, r5
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;INT 2 STRING - converts an int to a string
-;	input - r1 - int to convert
-;		  - r0 - address to strore string as null terminalted
-;----------------------------------------------------------------
-int2string:
-	PUSH {r4-r12,lr}
-	MOV r4, r1	;Storing the original integer
-	MOV r5, r0	;Storing the Strings address
-	CMP r4, #0 ;comparing r4 to zero to see if we need to deal with -
-    BGE INT2STRING_NEG_SKIP
-    EOR r4, r4, #0xFFFFFFFF ;flips bits for the string output to make it easy
-    ADD r4, r4, #1 ;adds 1 for twos comp
-    MOV r1, #0x2D;this is a - for negative
-    STRB r1, [r5], #1 ;store the string character to string address
-INT2STRING_NEG_SKIP:
-	;divide by 100 to start
-	MOV r0, r4 ;setting divised
-	MOV r1, #100;setting divisor ;it will allow us to get the least sig decimal and change it one by one into strings
-	BL div_and_mod
-	ADD r0, r0, #48;takes divided and adjusts the value to represent the character so we can store this number correctly
-	MOV r4, r1;this is the division output to continue the cycle
-	STRB r0, [r5], #1;store the string character to string address
-	;Now divide by 10 to get our last 2 digits
-	MOV r0, r4 ;setting divised
-	MOV r1, #10;setting divisor ;it will allow us to get the least sig decimal and change it one by one into strings
-	BL div_and_mod
-	ADD r0, r0, #48;takes divided counter and adjusts the value to represent the character
-	ADD r1, r1, #48;takes remainder and adjusts the value to represent the character
-	STRB r0, [r5], #1;store the string character to string address
-	STRB r1, [r5], #1;store the string character to string address
-	MOV r4, #0x00
-	STRB r4, [r5];stores a NULL at the string address to stop the end of the string
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;STRING 2 INT - converts the NULL terminated ASCII string pointed
-;to by the address passed into the routine in r0 to an integer.
-;The integer should be returned in r0
-;----------------------------------------------------------------
-string2int:
-	PUSH {r4-r12,lr} ; Store any registers in the range of r4 through r12
-	MOV r4, r0 		;Address of passed through string
-	MOV r5,#1
-	MOV r10, #10
-	MOV r6 , #0 	;accumnator
-	SUB r4,r4,#1
-negFlag:
-	EOR r5, #1		;neg flag
-	ADD r4,r4,#1
-stringIntLoop:
-	LDRB r0, [r4], #1
-	CMP r0, #00		;Check for null terminator
-	BEQ ExitstringIntLoop
-	STRB r0, [r4]	;load char
-	CMP r0, #0x2D	;check for neg
-	BEQ negFlag
-  	SUB r0, r0, #0x30
-  	MUL r6, r6, r10
-  	ADD r6,r6, r0
-	B stringIntLoop
-ExitstringIntLoop:
-	CMP r5, #0x01
-	BNE stringIntSkip
-	EOR	r6, r6, #0xFFFFFFFF
-	ADD  r6, r6, #1
-stringIntSkip:
-	MOV r0, r6
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;portINIT - SUBroutine given 1-6(A->F) to represent ports
-; The function returns the address of the port in R1
-;----------------------------------------------------------------
-portINIT:
-	PUSH {r4-r12,lr}
-	CMP r0, #0
-	BLE ERRORFOUND
 	CMP r0, #4
-	BLE A2D
-	CMP r0, #6
-	BEQ E2F
-	BL ERRORFOUND;in the case that we passed in a bad choice
-A2D:
-	MOV r1, #0x4000 ; Getting port D loaded up
-	ADD r1,r1 ,r0, LSL #12
-	MOVT r1, #0x4000
-	B FINALportINIT
-E2F:
-	MOV r1, #0x0000 ; Getting port F loaded up
-	ADD r1,r1 ,r0, LSL #3
-	MOVT r1, #0x4002
-	B FINALportINIT
-FINALportINIT:;comes here once we are all done and we use r1 as the port address
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	BEQ sisterButtonsBitOUT
+
+	CMP r0, #5
+	BEQ exitRoutine
+	B ERRORFOUND
 
 
+colourSelector:
+;Print list of colours, get usr input and update the led to the appropriate colour
+	BL colorPromptOUT
 
-;----------------------------------------------------------------
-;ERRORFOUND - creates an infinite loop that will never finish
-;----------------------------------------------------------------
-
-ERRORFOUND:
-	LDR r0, ptr_to_errorPrompt
-	BL output_string;the prompt being printed is in r0
 	B LOOP
 
-;================================================================
+onBoardLEDsOUT:
 
-;----------------------------------------------------------------
-;Uart init - handles setting up connection to Uart
-;----------------------------------------------------------------
-uart_init:
-	PUSH {r4-r12,lr}
-    MOV r4, #0xE618
-	MOVT r4,#0x400F
-	MOV r5, #1
-	STR r5,[r4]		;Provide clock to UART0
-	MOV r4, #0xE608
-	MOVT r4,#0x400F
-	MOV r5, #1
-	STR r5,[r4]		;Enable clock to PortA
-	MOV r4, #0xC030
-	MOVT r4,#0x4000
-	MOV r5, #0
-	STR r5, [r4]		; Disable UART0 Control
-	MOV r4, #0xC024
-	MOVT r4,#0x4000
-	MOV r5, #8
-	STR r5,[r4]		; Set UART0_IBRD_R for 115,200 baud
-	MOV r4, #0xC028
-	MOVT r4,#0x4000
-	MOV r5, #44
-	STR  r5,[r4]		; Set UART0_FBRD_R for 115,200 baud
-	MOV r4, #0xCFC8
-	MOVT r4,#0x4000
-	MOV r5, #0
-	STR r5,[r4]		; * Use System Clock
-	MOV r4, #0xC02C
-	MOVT r4,#0x4000
-	MOV r5, #0x60
-	STR r5,[r4]		;Use 8-bit word length, 1 stop bit, no parity
-	MOV r4, #0xC030
-	MOVT r4,#0x4000
-	MOV r5,#0x301
-	STR  r5,[r4]		;Enable UART0 Control
-	;OR operational setup
-	MOV r4,#0x451C
-	MOVT r4, #0x4000	;Load Memory address
-	LDR r5, [r4] ;make P0 and PA1 as a Digital Ports
-	ORR r5, r5, #0x03
-	STR r5, [r4]
-	MOV r4,#0x4420
-	MOVT r4, #0x4000
-	LDR r5, [r4] ;change P0, PA1 to use alternate function
-	ORR r5, r5, #0x03
-	STR r5, [r4]
-	MOV r4,#0x452C
-	MOVT r4, #0x4000
-	LDR r5, [r4] ;configure P0, PA1 for UART
-	ORR r5, r5, #0x11
-	STR r5, [r4]
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
+	B LOOP
 
-;----------------------------------------------------------------
-;Output Char - Takes an ascii byte in r0 and outputs to terminal
-;----------------------------------------------------------------
-output_character:
-	PUSH {r4-r12,lr}
-	MOV r4,#0xC000
-    MOVT r4, #0x4000	;Load Memory address into r4
-WAIT2OUTPUT:			;loop to keep waiting for flag to be flipped
-	LDRB r5, [r4, #U0FR];load uart flag registor into r5
-	AND r5, #32
-	CMP r5, #0
-	BNE WAIT2OUTPUT	;if flag is not flipped, keep waiting
-	STRB r0, [r4]	;store the byte to ouptut in uart Data registor
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;Read Char - Takes ascii char from uart and stores in r0
-;----------------------------------------------------------------
-read_character:
-	PUSH {r4-r12,lr}
-    MOV r4,#0xC000
-	MOVT r4, #0x4000	;Load Memory address into r4
-WAIT4INPUT:
-	LDRB r0, [r4, #U0FR];Flag register
-	AND r0, r0, #0x0010;checks RxFE
-	CMP r0, #0;checks if 0
-	BNE WAIT4INPUT
-	LDRB r0, [r4] ;load char from uart data reg
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;Read String - Takes address in r0 and stores a string from term
-;at memory location null terminated
-;----------------------------------------------------------------
-read_string:
-	PUSH {r4-r12,lr}
-    MOV r4, r0 ;Address of passed through string
-READLOOP:
-	BL read_character
-	CMP r0, #13
-	BEQ EXITREAD
-	STRB r0, [r4], #1
-	B READLOOP
-EXITREAD:
-	MOV r0, #0
-	STRB r0, [r4]
-	POP {r4-r12,lr}
-	MOV pc, lr
-;================================================================
-
-;----------------------------------------------------------------
-;Output String -
-;input	- r0 - address of null terminated string
-;	  	- r1 - new line conidtion - set to neg value to turn off
-;string stored at memory location to term
-;----------------------------------------------------------------
-output_string:
-	PUSH {r4-r12,lr}
-	MOV r4,r0
-PRINTLOOP:
- 	LDRB r0, [r4],#1
-	CMP r0, #0x00
-	BEQ PRINTEXIT
-	BL output_character
-	B PRINTLOOP
-PRINTEXIT:
-	cmp r1 , #0
-	BGT NONEWLINE
-	MOV r1,#1
-	LDR r0, ptr_to_newLine
+buttonBitOUT:
+;Prints out where button was printed was
+	LDR r0, ptr_to_btnPrompt
+	MVN r1, #1					;new line at end of str
 	BL output_string
-NONEWLINE:
+	BL read_tiva_push_button
+	CMP r0, #1
+	BEQ BUTTONPUSHED
+	LDR r0, ptr_to_btnPressed
+	B EXITBUTTONPUSHED
+BUTTONPUSHED:
+	LDR r0, ptr_to_btnNotPressed
+	B EXITBUTTONPUSHED
+EXITBUTTONPUSHED:
+	MVN r1, #1					;new line at end of str
+	BL output_string
+	B LOOP
+sisterButtonsBitOUT:
+	B LOOP
+
+exitRoutine:
+ENDOFLAB:
+	LDR r0, ptr_to_extmsg
+	MVN r1, #1				;new line at end of str
+	BL output_string
+	POP {r4-r12,lr}
+	MOV pc, lr
+
+
+;----------------------------------------------------------------
+;colorPrompt - Prints the colorPrompt and reads a string from the user
+;outputs a color number for the RGB lights to use
+;----------------------------------------------------------------
+colorPromptOUT:
+	PUSH {r4-r12,lr} 				; Store any registers in the range of r4 through r12
+	LDR r0, ptr_to_colorPrompt
+	MVN r1, #1
+	BL output_string				;the prompt being printed is in r0
+	LDR r0, ptr_to_usrInput
+	BL read_string					;the number we want is in r0
+	LDR r0, ptr_to_usrInput
+	BL string2int					; the integer will be in r0
+	MOV r4, r0 						;KEEP SAFE
+
+	;Now that we have the users choice we are gonna print it out on the board
+	MOV r0, #6
+	BL PORTINIT						;address we want in r1
+	MOV r0, r4						;color number we want
+	
+	;Now light up
+	BL illuminate_RGB_LED
+
 	POP {r4-r12,lr}
 	MOV pc, lr
 ;================================================================
 
-	.end
+.end

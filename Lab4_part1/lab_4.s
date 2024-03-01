fdf96ad19e2d2a67625ad45603a108299fabac64
ThomasJM03 <99485764+ThomasJM03@users.noreply.github.com>
2024-03-01 09:33:46 -0500

    Update lab_4.s

--- Lab4_part1/lab_4.s
+++ Lab4_part1/lab_4.s
@@ -16,41 +16,66 @@
 
 
 colorPrompt:	.string "Enter number for color:\r\nWhite: 1\r\nRed: 2\r\nGreen: 3\r\nBlue: 4\r\nPurple: 5\r\nYellow: 6\r\n", 0
+programPrompt   .string "Enter number for program:\r\nilluminate_RGB_LED: 1\r\nilluminate_LEDs: 2\r\nread_tiva_push_button: 3\r\nread_from_push_btns: 4\r\n",0
 startPrompt:	.string "Lab 4 - Tom and Tim",0
+answers			.string "Store answers here",0
 askRunAgain:	.string "Would you like to run again Yes(Y) No(N)?", 0
 extmsg:			.string "End of program ※\(^o^)/※", 0
 
 	.text
 
 ptr_to_colorPrompt:		.word colorPrompt
+ptr_to_programPrompt:	.word programPrompt
 ptr_to_startPromt:		.word startPrompt
+ptr_to_answers:			.word answers
 ptr_to_runAgin:			.word askRunAgain
 ptr_to_extmsg:			.word extmsg
 
 lab4:
 	PUSH {r4-r12,lr}
+	;INIT
 	BL uart_init
-	LDR r0, ptr_to_startPromt
-	MVN r1,# 1				;new line at end of str
-	BL output_string
-	;TESTING LOOP
 	BL gpio_btn_and_LED_init
+
+	LDR r0, ptr_to_startPromt
+	MVN r1, #1					;new line at end of str
+	BL output_string
+
+	;We want to give the user the option to test functionality
 LOOP:
-	BL read_tiva_push_button
-	MOV r1,r0
-	BL read_from_push_btns
-	;BL colorPromptOUT
-	MOV r0, #4
-	BL illuminate_RGB_LED
-	BL illuminate_LEDs
-	B LOOP
-	POP {r4-r12,lr}
-	MOV pc, lr
+	LDR r0, ptr_to_programPrompt
+	MOV r1, #0					;new line at end of str
+	BL output_string
+	LDR r0, ptr_to_answers		;Store the user input here
+	BL read_string				;finds their choice
+	LDR r0, ptr_to_answers		;loads user input before converting it to int
+	BL string2int				;return number and stores it in R0
+
+	CMP r0, #1
+	BEQ colorpromptOUT
+
+	CMP r0, #2
+	BEQ onBoardLEDsOUT
+
+	CMP r0, #3
+	BEQ buttonBitOUT
+	B	sisterButtonsBitOUT
+
+	B LOOP;resets
+
+colorPromptOUT:
+onBoardLEDsOUT:
+onBoardLEDsOUT:
+sisterButtonsBitOUT:
+
 
 ENDOFLAB:
 	LDR r0, ptr_to_extmsg
 	MVN r1, #1				;new line at end of str
 	BL output_string
+
+	POP {r4-r12,lr}
+	MOV pc, lr
 
 
 

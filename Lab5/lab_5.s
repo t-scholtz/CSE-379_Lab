b6698a785f9ba75eb638a40a081ae244706c8717
TScholtz <tim.scholtz@outlook.com>
2024-03-05 17:39:01 -0500

    got to compile but trouble running

--- Lab5/lab_5.s
+++ Lab5/lab_5.s
@@ -4,14 +4,14 @@
 	.global mydata
 
 prompt:	.string "Your prompt with instructions is place here", 0
-mydata:	.byte	0x20	; This is where you can store data. 
+mydata:	.byte	0x20	; This is where you can store data.
 			; The .byte assembler directive stores a byte
-			; (initialized to 0x20) at the label mydata.  
-			; Halfwords & Words can be stored using the 
-			; directives .half & .word 
+			; (initialized to 0x20) at the label mydata.
+			; Halfwords & Words can be stored using the
+			; directives .half & .word
 
 	.text
-	
+
 	.global uart_interrupt_init
 	.global gpio_interrupt_init
 	.global UART0_Handler
@@ -23,77 +23,154 @@ mydata:	.byte	0x20	; This is where you can store data.
 	.global output_string			; This is from your Lab #4 Library
 	.global uart_init					; This is from your Lab #4 Library
 	.global lab5
-	
+	.global portINIT
+	.global LOOP
+
 ptr_to_prompt:		.word prompt
 ptr_to_mydata:		.word mydata
 
-lab5:								; This is your main routine which is called from 
-; your C wrapper.  
+UARTIM: 			.equ 0x038		; UARTIM offset
+ENO:				.equ 0x100		;Enable pin interupt offset
+GPIOIS: 			.equ 0x404	;GPIO Interrupt Sense Register
+GPIOIBE:			.equ 0x408 	;GPIO Interrupt Both Edges Register
+GPIOIV:				.equ 0x40C	;GPIO Interrupt Event Register
+GPIOIM:				.equ 0x410	;GPIO Interrupt Mask Register
+GPIOICR:			.equ 0x41C	;GPIO Interrupt Clear Register
+
+
+lab5:								; This is your main routine which is called from
+; your C wrapper.
 	PUSH {r4-r12,lr}   		; Preserve registers to adhere to the AAPCS
-	ldr r4, ptr_to_prompt
-	ldr r5, ptr_to_mydata
+
 
  	bl uart_init
 	bl uart_interrupt_init
 	bl gpio_interrupt_init
 
-	; This is where you should implement a loop, waiting for the user to 
+	ldr r4, ptr_to_prompt
+	ldr r5, ptr_to_mydata
+
+LOOP:
+
+	BL LOOP
+	; This is where you should implement a loop, waiting for the user to
 	; enter a q, indicating they want to end the program.
- 
+
 	POP {lr}		; Restore registers to adhere to the AAPCS
 	MOV pc, lr
 
 
 
+;----------------------------------------------------------------
+;uart_interrupt_init: This subroutine will generate an interupt when a keystroke is generated
+; by a user in PUTTY
+;----------------------------------------------------------------
 uart_interrupt_init:
-		
+
+	MOV r0, #0xc0000 ;This is the UART Base address
+	MOVT r0, #0x4000
+
+							;We need to access bit 5 at the UARTIM position
+	LDR r1, [r0, #UARTIM]	;This loads the base value of the UARTIM data and we need to update it
+	ORR r1, r1, #32			;Now we have the updated value to store back
+	STR r1, [r0, #UARTIM]
+
+							;Now we need to set the ENABLE pin
+	MOV r0, #0xE0000 		;This is the UART Base address
+	MOVT r0, #0xE000
+
+							;We need to access bit 5 at the ENABLE position
+	LDR r1, [r0, #UARTIM]	;This loads the base value of the ENABLE data and we need to update it
+	ORR r1, r1, #32			;Now we have the updated value to store back
+	STR r1, [r0, #UARTIM]
+
+							;NOW both the UART should be set to take interupts along with the enable pin to allow the interups
+
+
+
 	; Your code to initialize the UART0 interrupt goes here
 
 	MOV pc, lr
+;================================================================
 
-
+;----------------------------------------------------------------
+;gpio_interrupt_init - initliase interupt for sw1
+;----------------------------------------------------------------
 gpio_interrupt_init:
-		
-	; Your code to initialize the SW1 interrupt goes here
+	PUSH {r4-r12,lr}
+	MOV r0,#5		;load port f
+	BL portINIT
+	LDRB r1, [r0,#GPIOIS]
+	AND r1,r1,#0xF7
+	STRB r1, [r0,#GPIOIS]
+	LDRB r1, [r0,#GPIOIBE]	;set to trigger on single edge change
+	AND r1,r1,#0xF7
+	STRB r1, [r0,#GPIOIBE]
+	LDRB r1, [r0,#GPIOIV]	;set to trigger on falling edge
+	AND r1,r1,#0xF7
+	STRB r1, [r0,#GPIOIV]
+	LDRB r1, [r0,#GPIOIM]	;set to trigger on falling edge
+	AND r1,r1,#0xFF
+	STRB r1, [r0,#GPIOIM]
+	MOV r0, #0xE000
+	MOVT r0, #0xE000
+	LDR r1, [r0,#0x100]
+	MOVT r2, #0x4000
+	ORR r1,r1,r2
+	STR r1, [r0,#0x100]
+
 	; Don't forget to follow the procedure you followed in Lab #4
 	; to initialize SW1.
-
+	POP {r4-r12,lr}
 	MOV pc, lr
+;================================================================
 
+UART0_Handler:
 
-UART0_Handler: 
-	
 	; Your code for your UART handler goes here.
-	; Remember to preserver registers r4-r11 by pushing then popping 
+	; Remember to preserver registers r4-r11 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler
 
 	BX lr       	; Return
 
-
+;----------------------------------------------------------------
+;Switch_Handler - handles interupt for sw1 being pressed
+;----------------------------------------------------------------
 Switch_Handler:
-	
-	; Your code for your UART handler goes here.
-	; Remember to preserver registers r4-r11 by pushing then popping 
-	; them to & from the stack at the beginning & end of the handler
+	PUSH {r0-r12,lr}
+	;Clear interupt value
+	MOV r0, #0x5000
+	MOVT r0, #0x4002
+	LDRB r1, [r0,#GPIOICR]
+	ORR r1,r1,#0x08
+	STRB r1, [r0,#0x100]
 
+	;print to screen btn was pushed
+	LDR r0, ptr_to_prompt
+	BL output_string
+
+
+
+	POP {r0-r12,lr}
 	BX lr       	; Return
+;================================================================
 
 
 Timer_Handler:
-	
+
 	; Your code for your Timer handler goes here.  It is not needed for
 	; Lab #5, but will be used in Lab #6.  It is referenced here because
 	; the interrupt enabled startup code has declared Timer_Handler.
-	; This will allow you to not have to redownload startup code for 
+	; This will allow you to not have to redownload startup code for
 	; Lab #6.  Instead, you can use the same startup code as for Lab #5.
-	; Remember to preserver registers r4-r11 by pushing then popping 
+	; Remember to preserver registers r4-r11 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler.
 
 	BX lr       	; Return
 
 
-simple_read_character: 
-	
+simple_read_character:
+
 	MOV pc, lr	; Return
 
 	.end

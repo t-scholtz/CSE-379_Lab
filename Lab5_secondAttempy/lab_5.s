a8def44c1b5ac507c746fe6d23a1c979a4ecbf6b
ThomasJM03 <99485764+ThomasJM03@users.noreply.github.com>
2024-03-09 19:40:55 -0500

    Update lab_5.s
    Made decent length on the game and created a state change to handle the 4 compares. The error occurs before this subroutine came into place. I wonder is BX is causing lines of code to be skipped.

--- Lab5_secondAttempy/lab_5.s
+++ Lab5_secondAttempy/lab_5.s
@@ -22,14 +22,17 @@
 
 ;PROGRAM DATA
 ;================================================================
-startUpPrompt:	.string "Hello! Lab 5 - Tom and Tim",0
-intructions:	.string "Game intructions go here but tim is too lazt to write them" , 0
+startUpPrompt:	.string 0x0D, 0x0A, "Hello! Lab 5 - Tom and Tim",0
+intructions:	.string 0x0D, 0x0A, "Game intructions go here but tim is too lazt to write them" , 0
 waiting:		.string "Waiting: "
 press:			.string "<<< Press to Start >>>",0
+ready:			.string 0x0D, 0x0A, "READY?..."
+set:			.string 0x0D, 0x0A, "SET!..."
+gogo:			.string 0x0D, 0x0A, "GOOOOO!!!"
 state:			.byte	0x01
 p1Score:		.byte	0x00
 p2Score:		.byte	0x00
-disqualified:	.byte	0x00
+disqualified:	.byte	0x00 ;INSTEAD OF HANFLING IT HERE I AM GOING TO HAVE IT AT THE BOTTOM
 space:			.string "space" ,0
 
 ;================================================================
@@ -41,6 +44,9 @@ ptr_to_startUpPrompt:	.word startUpPrompt
 ptr_to_intructions:		.word intructions
 ptr_to_waiting:			.word waiting
 ptr_to_press:			.word press
+ptr_to_ready:			.word ready
+ptr_to_set:				.word set
+ptr_to_gogo:			.word gogo
 ptr_to_state:			.word state
 ptr_to_p1Score:			.word p1Score
 ptr_to_p2Score:			.word p2Score
@@ -50,14 +56,15 @@ ptr_to_space:			.word space
 
 ;LIST OF CONSTANTS
 ;================================================================
-UARTIM: 			.equ 0x038	; UARTIM offset
-UARTICR:			.equ 0x044  ;Interrupt clear register
-ENO:				.equ 0x100	;Enable pin interupt offset
-GPIOIS: 			.equ 0x404	;GPIO Interrupt Sense Register
-GPIOIBE:			.equ 0x408 	;GPIO Interrupt Both Edges Register
-GPIOIV:				.equ 0x40C	;GPIO Interrupt Event Register
-GPIOIM:				.equ 0x410	;GPIO Interrupt Mask Register
-GPIOICR:			.equ 0x41C	;GPIO Interrupt Clear Register
+UARTIM: 			.equ 0x038		; UARTIM offset
+UARTICR:			.equ 0x044  	;Interrupt clear register
+ENO:				.equ 0x100		;Enable pin interupt offset
+GPIOIS: 			.equ 0x404		;GPIO Interrupt Sense Register
+GPIOIBE:			.equ 0x408 		;GPIO Interrupt Both Edges Register
+GPIOIV:				.equ 0x40C		;GPIO Interrupt Event Register
+GPIOIM:				.equ 0x410		;GPIO Interrupt Mask Register
+GPIOICR:			.equ 0x41C		;GPIO Interrupt Clear Register
+TIMER:				.equ 0xB2D05E00 ;THIS will be the timer for the countDOWN
 ;================================================================
 
 
@@ -81,6 +88,11 @@ lab5:								; This is your main routine which is called from
 	MVN r1, #1
 	BL output_string
 
+	;SETTING up the clock for later
+	MOV r10, #0x5E00
+	MOVT r10, #0xB2D0
+	;This should give us a healthy clock of 3 billion(TEMP)
+
 ;~~~~~~~~~~~~~~~~~~~~~~~~~
 ;Game state table
 ;	1 - start mode
@@ -89,24 +101,32 @@ lab5:								; This is your main routine which is called from
 ;	4 - calculation mode (don't accept input)
 ;~~~~~~~~~~~~~~~~~~~~~~~~~
 GAMELOOP:
+
 instructions:			;Print Instructions to screen
+
 	MOV r4,#1
 	LDR r5, ptr_to_state;set Program state to 1
 	STRB r4, [r5]
 	LDR r0, ptr_to_intructions
 	MVN r1, #1
 	BL output_string
-waitforgametostart:		;waiting for user to press enter tostart the game
+
+waitforgametostart:	;waiting for user to press enter tostart the game
+
 	MOV r3, #0			;counter
 	MOV r1, #0x01
+
 waitforgameLoop:		;wait for usr input loop - prints scrolling text to show program still running
+
 	MOV r0, #0x0D
 	MOV r1, #1
 	BL output_character	;print cariage return
 	LDR r0, ptr_to_waiting
 	BL output_string
 	MOV r4, #0
+
 printPressLoop:
+
 	ADD r4,r4,#1
 	MOV r0, #0x20
 	BL output_character
@@ -122,19 +142,69 @@ printPressLoop:
 	MOV r7,#0			;count to a big number
 	MOVT r7, #0x000F
 	MOV r8, #0
-burn:
+burn:					;TIM: More comments would be appricated to narrate your thought process when you are not with me coding
+
+	;TOMS CODE, I am adding a check because it seems to get stuck here after the first state needs to change
+	LDR r0, ptr_to_state
+	LDRB r1, [r0]		;state of program is stored in r1
+	CMP r1, #2 			;This will let us know when it needs to switch
+	BEQ game_startLOOP
+	;TOMS CODE END, this is to make sure it switches to the countdown state
+
+
 	add r8,r8, #1
 	NOP
 	CMP r8,r7
 	BLE burn
 	B waitforgameLoop
 
+game_startLOOP: 		;This is gonna be the point where we do the READY... SET... GO...
+;R10 will be reseved for clock time
+;r9 will be the comparing adress represeing a huge number for the clock
 
+	;First print READY 3 billion
+	MOV r9,  #0x5E00
+	MOVT r9, #0xB2D0
+	CMP r10, r9
+	BEQ print_ready
+	;HALF DONE print SET!, 1.5 billion
+	MOV r9,  #0x2F00
+	MOVT r9, #0x5968
+	CMP r10, r9
+	BEQ print_set
+	;DONE print gogo!!! 0 billion
+	CMP r10, #0x0
+	BEQ print_go
+	;CHECK state
+	LDR r0, ptr_to_state
+	LDRB r1, [r0]
+	CMP r1, #3
+	BEQ press_and_read
+	;FINALLY just sub 1 and branch
+	SUB r10, r10, #1
+	B game_startLOOP
 
+print_ready:
+	LDR r0, ptr_to_ready
+	MVN r1, #1
+	BL output_string
+	B game_startLOOP
 
+print_set:
+	LDR r0, ptr_to_set
+	MVN r1, #1
+	BL output_string
+	B game_startLOOP
 
+print_go:
+	LDR r0, ptr_to_gogo
+	MVN r1, #1
+	BL output_string
+	B game_startLOOP
 
-	; This is where you should implement a loop, waiting for the user to
+press_and_read:
+
+	; This is where you should implement a loop, waiting for the u  ser to
 	; enter a q, indicating they want to end the program.
 
 	POP {lr}		; Restore registers to adhere to the AAPCS
@@ -198,8 +268,8 @@ gpio_interrupt_init:
 
 
 ;----------------------------------------------------------------
-;UART0_Handler - This is the interupt that is ran Once a signal from the UART ex
-;----------------------------------------------------------------
+;UART0_Handler - This is the interupt that is ran Once a signal f rom the UART ex
+ ;----------------------------------------------------------------
 UART0_Handler:
 	PUSH {r0-r12,lr}
 	LDR r0, ptr_to_state
@@ -239,13 +309,8 @@ Switch_Handler:
 	LDR r0, ptr_to_state
 	LDRB r1, [r0]		;state of program is stored in r1
 
-	CMP r1, #1
+	BL state_changer 	;Have all of the state discitions moved to here
 
-	CMP r1, #2
-
-	CMP r1, #3
-
-	CMP r1, #4
 	;Clear interupt value
 	MOV r0, #0x5000
 	MOVT r0, #0x4002
@@ -257,6 +322,50 @@ Switch_Handler:
 	BX lr       	; Return
 ;================================================================
 
+;----------------------------------------------------------------
+;state_changer - Takes in current state in r1
+; 				 Then alters it depending on certian copnditions
+;---------------------------------------------------------------
+state_changer:
+	PUSH {r4-r12,lr}
+	;IF you are currently in state one
+	CMP r1, #1
+	BEQ FIRST_S
+
+	;IF you are currently in state two
+	CMP r1, #2
+	BEQ SECOND_S
+
+	;IF you are currently in state three
+	CMP r1, #3
+	BEQ THIRD_S
+
+	;IF you are currently in state four
+	CMP r1, #4
+	BEQ FOURTH_S
+
+FIRST_S:
+	;Currently they should be in the press to start portion
+	;All that is needed here is the to move up to the next
+	MOV r1, #2
+	STRB r1, [r0]				;This will alter the value of the state
+	;MOV  pc, game_startLOOP		;I don't know if this will update the program counter but I need to update the location
+	BL FINISH_state_changer
+
+SECOND_S:
+	BL FINISH_state_changer
+
+THIRD_S:
+	BL FINISH_state_changer
+
+FOURTH_S:
+	BL FINISH_state_changer
+
+FINISH_state_changer:
+
+	POP {r4-r12,lr}
+	MOV pc, lr
+;================================================================
 Timer_Handler:
 
 	; Your code for your Timer handler goes here.  It is not needed for

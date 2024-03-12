c7110ce22678106977086139835d47adedde402f
TScholtz <tim.scholtz@outlook.com>
2024-03-12 12:32:21 -0400

    Init lab-6 docs

--- lab6/lab_6.s
+++ lab6/lab_6.s
@@ -25,8 +25,28 @@
 ;================================================================
 startUpPrompt:	.string 0x0D, 0x0A, "Hello! Lab 6 - Tom and Tim",0
 scorePrompt:	.string "Score: ",0
-gameBoard:		.string "TODO",
-				.string "string can go over mulpitle labs
+gameBoard:		.string "----------------------",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "|                    |",0x0D, 0x0A,
+				.string "----------------------",0x0D, 0x0A,0
 playerDir:		.byte	0x00	; 0 up 1 right 2 down 3 left
 xPos:			.byte	0x0A
 yPos:			.byte	0x0A
@@ -43,7 +63,6 @@ ptr_to_playerDir:		.word playerDir
 ptr_to_xPos:			.word xPos
 ptr_to_yPos:			.word ypos
 ptr_to_score:			.word score
-
 ;================================================================
 
 ;LIST OF CONSTANTS
@@ -58,7 +77,6 @@ GPIOIM:				.equ 0x410		;GPIO Interrupt Mask Register
 GPIOICR:			.equ 0x41C		;GPIO Interrupt Clear Register
 TIMER:				.equ 0xB2D05E00 ;THIS will be the timer for the countDOWN
 ;================================================================
-
 
 ;CODE
 ;++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -75,21 +93,42 @@ lab5:								; This is your main routine which is called from
 	bl gpio_btn_and_LED_init
 	bl uart_interrupt_init
 	bl gpio_interrupt_init
+	bl timer_init
 	;Start up sequence
 
+progLoop:
 
+	b progLoop				;burn condition
 
 END_PROGRAM:
 	POP {r4-r12,lr} 		; Restore registers to adhere to the AAPCS
 	MOV pc, lr
 ;================================================================
 
+;----------------------------------------------------------------
+;update_screen - re-draws the screen with update values
+;----------------------------------------------------------------
+update_screen:
+	PUSH {r4-r12,lr}
+	;check the player position and directinos
+	;check that game hasn't ended -> if ended handle
+	;draw the new * on the map
+	;update the score
+	;draw score and map to screen
+
+	POP {r4-r12,lr}
+	MOV pc, lr
+;================================================================
 
 ;----------------------------------------------------------------
 ;UART0_Handler - This is the interupt that is ran Once a signal f rom the UART ex
- ;----------------------------------------------------------------
+;----------------------------------------------------------------
 UART0_Handler:
 	PUSH {r0-r11,lr}
+
+
+	;Uart handler needs to take in input, check if the WASD/ arrows keys were pressed and then update player direction
+
 
 EXIT_UART_HANDLER:
 	POP {r0-r11,lr}
@@ -108,7 +147,34 @@ EXIT_SWITCH_HANDLER
 
 ;================================================================
 
+;----------------------------------------------------------------
+;timer_init - conencts the timer to the interupt handler
+;	takes no input
+;----------------------------------------------------------------
+timer_init:
+	PUSH {r4-r12,lr}
+	;Connect Clock to Timer
+	MOV r4, #0xE000
+	MOVT r4, #0x400F
+	MOV r5, #0x604
+	LDR r6, [r4,r5]
+	ORR r6, #0x01
+	STR r6, [r4,r5]
+	;Configure Timer
+	MOV r4, #0
+	MOVT r4,#0x4003
+	MOV r5, #0x00C
+	;Enable Timer Interrupt
+	;Enable Timer
 
+
+
+
+	POP {r4-r12,lr}
+	MOV pc, lr
+;----------------------------------------------------------------
+;Timer_Handler
+;----------------------------------------------------------------
 Timer_Handler:
 
 	; Your code for your Timer handler goes here.  It is not needed for
@@ -122,6 +188,7 @@ Timer_Handler:
 	BX lr       	; Return
 
 
+;Not sure why this is here
 simple_read_character:
 
 	MOV pc, lr	; Return

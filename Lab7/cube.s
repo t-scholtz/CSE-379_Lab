a5068a3408bf8b292792e1f0a14302953621826a
ThomasJM03 <99485764+ThomasJM03@users.noreply.github.com>
2024-04-11 13:12:43 -0400

    Update cube.s
    We have not had a lot of chance to talk but here is a rough copy of loading up the cube.

--- Lab7/cube.s
+++ Lab7/cube.s
@@ -5,17 +5,37 @@
 ;We will have 4 bytes, up, left, down, right
 
 Direction_Cube:
-				.string 0x4,0x5,0x3,0x6;UP_1
-				.string 0x3,0x5,0x2,0x6;Bottom_2
-				.string 0x1,0x5,0x2,0x3;Front_3
-				.string 0x2,0x5,0x1,0x6;Back_4
-				.string 0x1,0x4,0x2,0x3;Left_5
-				.string 0x1,0x3,0x2,0x4;Right_6
+					.string 0x4,0x5,0x3,0x6;UP_1
+					.string 0x3,0x5,0x2,0x6;Bottom_2
+					.string 0x1,0x5,0x2,0x3;Front_3
+					.string 0x2,0x5,0x1,0x6;Back_4
+					.string 0x1,0x4,0x2,0x3;Left_5
+					.string 0x1,0x3,0x2,0x4;Right_6
 
+Face_generation:	.string 0x09,0x09,0x09,0x09,0x09,0x09	;This will help us realize if we are using a used face yet
+															;how many tiles are left on each face
+
+						   ;Green, Yellow, Blue, Pink, Turquoise, White, we allow this until they have a value of 9
+Color_Used:			.string 0x0,   0x0,    0x0,  0x0,  0x0,       0x0
+
+
+block_generation:		   ;1  ,2  ,3  ,4  ,5  ,6  ,7  ,8  ,9
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;UP_1
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;Bottom_2
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;Front_3
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;Back_4
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;Left_5
+					.string 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0;Right_6
+					.string 0x0 ;This is a finaly NULL incase we wanted it
+					;it will store the number of the color 1-6 Green, Yellow, Blue, Pink, Turquoise, White
 	.text
 
 ;POINTERS TO DATA
 ;================================================================
+ptr_to_Direction_Cube:					.word Direction_Cube
+ptr_to_Face_generation:					.word Face_generation
+ptr_to_Color_Used:						.word Color_Used
+ptr_to_block_generation:				.word block_generation
 
 
 
@@ -36,26 +56,22 @@ Direction_Cube:
 
 ;----------------------------------------------------------------
 ;Generate - Reads the clock to generate color, face, player start color, all use MOD 6
+;INPUT: NONE
+;OUTPUT: NONE-> but edits a lot of memory that is set above
+;USES: r8 -> adress of clock, r7 -> number of tile on, r6-> number of face on
 ;----------------------------------------------------------------
 	PUSH {r4-r12,lr}
 
-	;Make a loop that will load the face/ load tile/ Set tile
+	MOV r8,  #0x0050
+	MOVT r8, #0x4003	;this is the memory address of the clock value at any point in time
+	;MOV r7, #1			;#tiles per faces
+	MOV r6, #1			;#faces
+
+BIG_GEN:
+	BL load_face
+
 	;Once all faces are full pick a color for the player to start on
 
-	POP {r4-r12,lr}
-	MOV pc, lr
-
-;================================================================
-
-;----------------------------------------------------------------
-;load_face - reads the clock value and generates a face number, keeps looping until it finds availible face
-;			 Think about check the total number of tiles made so we can make sure this never infinite loops
-;
-;----------------------------------------------------------------
-	PUSH {r4-r12,lr}
-
-	;Check the number of tiles that has been made so far, if it is less than 54 than this is alowed to run
-	;Read the value for the clock, if tiles are availible on that face output int r1 the face using
 
 	POP {r4-r12,lr}
 	MOV pc, lr
@@ -63,14 +79,103 @@ Direction_Cube:
 ;================================================================
 
 ;----------------------------------------------------------------
-;load_tile - reads the clock value and generates a face number, keeps looping until it finds availible face
-;			 Think about check the total number of tiles made so we can make sure this never infinite loops
-;
+;load_face - keeps looping until it finds availible face, and until all faces are used up
+;INPUT: NONE
+;OUTPUT: NONE-> but edits a lot of memory that is set above
+;USES: r8 -> adress of clock, r7 -> number of tile on, r6-> number of faces left
 ;----------------------------------------------------------------
+load_face:
 	PUSH {r4-r12,lr}
 
-	;Check the number of tiles that has been made so far, if it is less than 54 than this is alowed to run
-	;Read the value for the clock, if tiles are availible on that face output int r1 the face using
+	;initializing the value so we can coundown and keep track
+
+LOADING_face_LOOP:
+	;doing a check to see if we can finish
+	MOV r7, #1						;#tiles per faces
+	CMP r6, #6
+	BEQ load_face_FINISH
+
+	;if there is a face left to fill keep going
+	BL load_tiles
+	ADD r6, r6, #1					;Once the tiles are loaded we take on face out of the equation
+
+	;Go back to the top until all faces are used
+	B LOADING_face_LOOP
+
+
+load_face_FINISH:
+
+	POP {r4-r12,lr}
+	MOV pc, lr
+
+;================================================================
+
+;----------------------------------------------------------------
+;load_tiles - loops through the tiles and generates and verify those colors can be used
+;INPUT: NONE
+;OUTPUT: NONE-> but edits a lot of memory that is set above
+;USES: r8 -> adress of clock, r7 -> , r6-> number of faces left,
+;	   r5-> memory address of the face we are on, r4->memory adress of what tile we are on in respect to face
+;	   r3-> the color of the tile, r2/r9-> is used for fast computations or load/storing
+;----------------------------------------------------------------
+load_tiles:
+	PUSH {r4-r12,lr}
+
+	;create a loop that will take you to the cube face and set that to r5
+	LDR r2, ptr_to_block_generation
+	LDRB r5, [r2]						;This will start us off in block 1 tile 1
+
+	MOV r2, r6							;puts the number of face into r2
+	MOV r9, #0x0						;Once this one get's to 9 the face is all done
+
+face_load_LOOP:
+	CMP r2, #1							;checks to see if we have decremented to the beggining
+	BEQ tile_load_LOOP
+
+	;if Not down to one, degrement the counter and increment the memory
+	SUB r2, r2, #1						;This is the counter to get us to the correct face
+	ADD r5, r5, #9 						;This will take up to the next face to work on
+	B face_load_LOOP
+
+	;create a loop that will take you to the cube tile and set that to r4
+tile_load_LOOP:
+	CMP r9, #0x9
+	IT EQ
+	MOVEQ r9, #0xFF						;this will be our ending flag
+	ADDNE r9, r9, #1
+		;Generate a color for the tile
+	BL Random_Gen
+		;store that color in mem
+	STRB r1, [r5]
+	;make sure we still need to loop
+	CMP r9, #0x9
+	ADD r5, r5, #1
+
+	BGT tile_load_FINISH
+
+tile_load_FINISH						;The entire face we are on should be complete
+
+
+	POP {r4-r12,lr} ;ASK the TA's and look at the notes if we can replce the lr with the pc like we learned in class
+	MOV pc, lr
+
+;================================================================
+
+;----------------------------------------------------------------
+;Random_Gen - reads the clock value and generates a number 1-6
+;Returns: 1-6 in r1
+;USES: r8 -> adress of clock, r7 -> , r6-> number of faces left,
+;	   r5-> memory address of the face we are on, r4->memory adress of what tile we are on in respect to face
+;	   r3-> the color of the tile, r2/r9-> is used for fast computations or load/storing
+;----------------------------------------------------------------
+Random_Gen:
+	PUSH {r4-r12,lr}
+
+	MOV r0, r8  		;divided
+	MOV r1, #6			;divisor
+
+	BL div_and_mod		;return a number 0-5 in r1
+	ADD r1, r1, #1		;This is adjusting out value to be 1-6
 
 	POP {r4-r12,lr}
 	MOV pc, lr
